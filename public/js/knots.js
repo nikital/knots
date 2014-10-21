6b7b7b3d57f14e8e694af421cfef493005701b2f
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-10-21 23:25:42 +0300

    GUI for real!
    Not really

--- public/js/knots.js
+++ public/js/knots.js
@@ -7,10 +7,25 @@ function Knots() {
     this._tie_btn = document.getElementById('tie');
     this._climb_btn.onclick = this._onClimb.bind(this);
     this._tie_btn.onclick = this._onTie.bind(this);
+
+    this._self_height = document.getElementById('self-height');
+    this._self_knot = document.getElementById('self-knot');
+    this._other_height = document.getElementById('other-height');
+    this._other_knot = document.getElementById('other-knot');
+    this._rope_height = document.getElementById('rope-height');
 }
 
 Knots.prototype._onMessage = function(e) {
     console.log(e.data);
+    var data = JSON.parse(e.data);
+    this._self_height.value = data.self.height;
+    this._self_knot.value = data.self.max_knots;
+    this._other_height.value = data.other.height;
+    this._other_knot.value = data.other.max_knots;
+    this._rope_height.value = data.rope_height
+
+    this._climb_btn.disabled = !data.your_turn;
+    this._tie_btn.disabled = !data.your_turn;
 };
 
 Knots.prototype._onClimb = function() {
