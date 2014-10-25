3e80578d70abf42d44e613c9c8b63d30f12e3965
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-10-25 23:46:36 +0300

    Guya stage #0.0.1

--- public/js/knots.js
+++ public/js/knots.js
@@ -1,7 +1,25 @@
 function Knots() {
+    var ROPE_HEIGHT = 500;
+
     this._ws = new WebSocket('ws://' + window.location.host + '/player', 'knots');
     this._ws.onmessage = this._onMessage.bind(this);
     // TODO close and errors
+
+    this._stage = new createjs.Stage('knots');
+
+    this._rope_self = new Rope(ROPE_HEIGHT);
+    this._rope_self.x = 800/3;
+
+    this._rope_other = new Rope(ROPE_HEIGHT);
+    this._rope_other.x = 800/3*2;
+
+    this._steps = new Steps(ROPE_HEIGHT);
+    this._stage.addChild(this._steps);
+
+    this._stage.addChild(this._rope_self);
+    this._stage.addChild(this._rope_other);
+
+    this._steps.setSteps(5);
 
     this._climb_btn = document.getElementById('climb');
     this._tie_btn = document.getElementById('tie');
@@ -13,6 +31,8 @@ function Knots() {
     this._other_height = document.getElementById('other-height');
     this._other_knot = document.getElementById('other-knot');
     this._rope_height = document.getElementById('rope-height');
+
+    requestAnimationFrame(this._stage.update.bind(this._stage));
 }
 
 Knots.prototype._onMessage = function(e) {
@@ -42,6 +62,50 @@ Knots.prototype._onClimb = function() {
 
 Knots.prototype._onTie = function() {
     this._ws.send('tie');
+};
+
+function Rope(height) {
+    this.DisplayObject_initialize();
+
+    this._height = height;
+
+    this._rope = new createjs.Shape();
+    this._rope.graphics.
+        beginFill("black").
+        drawRect(-2, 0, 4, height);
+    this.addChild(this._rope);
+
+    this._player = new createjs.Shape();
+    this._player.graphics.
+        beginFill("red").
+        drawRect(-5, -5, 10, 10);
+    this._player.y = height - 20;
+    this.addChild(this._player);
+}
+
+Rope.prototype = new createjs.Container();
+Rope.prototype.constructor = Rope;
+
+function Steps(height) {
+    this.DisplayObject_initialize();
+    this._height = height;
+}
+
+Steps.prototype = new createjs.Shape();
+Steps.prototype.constructor = Steps;
+
+Steps.prototype.setSteps = function (steps) {
+    var i;
+
+    this.graphics.
+        clear().
+        setStrokeStyle(1).beginStroke("#666");
+
+    for (i = 1; i < steps; ++i) {
+        var h = this._height / steps * i;
+        this.graphics.
+            moveTo(0, h).lineTo(800, h);
+    }
 };
 
 var knots = new Knots();
