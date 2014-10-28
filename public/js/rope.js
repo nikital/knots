76667255631ecb4a8a28cc2f6c705aa717120cf0
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-10-28 23:28:16 +0200

    Verlet rope test

---
+++ public/js/rope.js
@@ -0,0 +1,122 @@
+var JOINT_DIST = 100;
+
+function Knots() {
+    var ROPE_HEIGHT = 500,
+        nodes = [];
+
+    this._stage = new createjs.Stage('rope');
+    this._rope = new createjs.Shape();
+    this._rope.x = 800 / 2;
+    this._rope.y = 20;
+    this._stage.addChild(this._rope);
+
+    for (var i = 0; i < 1 + ROPE_HEIGHT / JOINT_DIST; ++i) {
+        // nodes.push([i*i, i * JOINT_DIST]);
+        nodes.push([i*JOINT_DIST, 0]);
+    }
+    this._nodes = nodes;
+    this._prev_nodes = nodes;
+
+    this._onFrame();
+}
+
+Knots.prototype._onFrame = function() {
+    this._rope.graphics.clear().
+        setStrokeStyle(1).beginStroke('black');
+
+    this._simulate();
+
+    this._rope.graphics.moveTo(this._nodes[0][0], this._nodes[0][1]);
+    for (var i = 0; i < this._nodes.length; ++i) {
+        this._rope.graphics.lineTo(this._nodes[i][0], this._nodes[i][1]);
+        this._rope.graphics.drawCircle(this._nodes[i][0], this._nodes[i][1], 3);
+    }
+
+    this._stage.update();
+    requestAnimationFrame(this._onFrame.bind(this));
+};
+
+Knots.prototype._simulate = function() {
+    var prevN = this._prev_nodes;
+    var n = cloneNodes(this._nodes);
+
+    for (var i = 1; i < n.length; ++i) {
+        n[i][0] += (n[i][0] - prevN[i][0]) * 1;
+        n[i][1] += (n[i][1] - prevN[i][1]) * 1 + 7;
+    }
+
+    var newN = cloneNodes(n);
+
+    for (var i = 1; i < n.length; ++i) {
+        var p = n[i - 1],
+            c = n[i],
+            d, f, fx, fy, cnx, cny;
+
+        d = dist(c, p);
+        // JOINT_DIST = 0;
+        if ( d > JOINT_DIST) {
+            f = (d - JOINT_DIST) * 1;
+            cnx = (p[0] - c[0]) / d;
+            cny = (p[1] - c[1]) / d;
+            fx = cnx * f;
+            fy = cny * f;
+
+            newN[i][0] += fx;
+            newN[i][1] += fy;
+        }
+    }
+
+    this._prev_nodes = this._nodes;
+    this._nodes = newN;
+};
+
+// Knots.prototype._simulate = function() {
+//     var prevN = this._prev_nodes;
+//     var n = this._nodes;
+//     var newN = cloneNodes(n);
+//
+//     for(var i = 0; i < n.length; ++i){
+//         newN[i][0] += (newN[i][0] - prevN[i][0]) * 0.92;
+//         newN[i][1] += (newN[i][1] - prevN[i][1]) * 0.92;
+//     }
+//     
+//     for(var i = 1; i < n.length; ++i){
+//         
+//         var c = newN[i];
+//         var p = newN[i - 1];
+//         var d, f, fx, fy, cnx, cny;
+//         d = dist(c, p);
+//         if(d > JOINT_DIST){
+//             f = (d - JOINT_DIST) * 0.02;
+//             cnx = (p[0] - c[0]) / d;
+//             cny = (p[1] - c[1]) / d;
+//             fx = cnx * f;
+//             fy = cny * f;
+//
+//             newN[i][0] += fx;
+//             newN[i][1] += fy;
+//         }
+//
+//         newN[i][1] += 1;
+//     }
+//     
+//     this._prev_nodes = n;
+//     this._nodes = newN;
+// };
+
+function dist(p1, p2) {
+    var dx = p1[0] - p2[0],
+        dy = p1[1] - p2[1];
+
+    return Math.sqrt(dx*dx + dy*dy);
+}
+
+function cloneNodes(n) {
+    var newN = [];
+    for (var i = 0; i < n.length; ++i) {
+        newN.push([n[i][0], n[i][1]]);
+    }
+    return newN;
+}
+
+var knots = new Knots();
