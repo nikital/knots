1b9e83629ce303615fe15d543620da3f0d5645f4
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-10-30 00:23:26 +0200

    Rope kinda works, but still very bad

--- public/js/rope.js
+++ public/js/rope.js
@@ -6,18 +6,23 @@ function Knots() {
 
     this._stage = new createjs.Stage('rope');
     this._rope = new createjs.Shape();
-    this._rope.x = 800 / 2;
-    this._rope.y = 20;
     this._stage.addChild(this._rope);
 
     for (var i = 0; i < 1 + ROPE_HEIGHT / JOINT_DIST; ++i) {
         // nodes.push([i*i, i * JOINT_DIST]);
-        nodes.push([i*JOINT_DIST, 0]);
+        nodes.push([800/2 + i*JOINT_DIST, 20]);
     }
     this._nodes = nodes;
     this._prev_nodes = nodes;
 
+    this._stage.addEventListener('stagemousemove', this._onMouseOver.bind(this));
+    this._stage.enableMouseOver();
+
     this._onFrame();
+}
+
+Knots.prototype._onMouseOver = function(e) {
+    this._nodes[0] = [e.stageX, e.stageY];
 }
 
 Knots.prototype._onFrame = function() {
@@ -39,30 +44,43 @@ Knots.prototype._onFrame = function() {
 Knots.prototype._simulate = function() {
     var prevN = this._prev_nodes;
     var n = cloneNodes(this._nodes);
+    var newN;
 
     for (var i = 1; i < n.length; ++i) {
         n[i][0] += (n[i][0] - prevN[i][0]) * 1;
-        n[i][1] += (n[i][1] - prevN[i][1]) * 1 + 7;
+        n[i][1] += (n[i][1] - prevN[i][1]) * 1 + 1;
     }
 
-    var newN = cloneNodes(n);
+    newN = cloneNodes(n);
 
     for (var i = 1; i < n.length; ++i) {
-        var p = n[i - 1],
-            c = n[i],
-            d, f, fx, fy, cnx, cny;
+        var f;
 
-        d = dist(c, p);
-        // JOINT_DIST = 0;
-        if ( d > JOINT_DIST) {
-            f = (d - JOINT_DIST) * 1;
-            cnx = (p[0] - c[0]) / d;
-            cny = (p[1] - c[1]) / d;
-            fx = cnx * f;
-            fy = cny * f;
+        f = getSpringForce(n[i], n[i-1]);
+        newN[i][0] += f[0];
+        newN[i][1] += f[1];
 
-            newN[i][0] += fx;
-            newN[i][1] += fy;
+        if (i < n.length - 1) {
+            f = getSpringForce(n[i], n[i+1]);
+            newN[i][0] += f[0];
+            newN[i][1] += f[1];
+        }
+    }
+
+    n = newN;
+    newN = cloneNodes(n);
+
+    for (var i = 1; i < n.length; ++i) {
+        var f;
+
+        f = getSpringForce(n[i], n[i-1]);
+        newN[i][0] += f[0];
+        newN[i][1] += f[1];
+
+        if (i < n.length - 1) {
+            f = getSpringForce(n[i], n[i+1]);
+            newN[i][0] += f[0];
+            newN[i][1] += f[1];
         }
     }
 
@@ -117,6 +135,24 @@ function cloneNodes(n) {
         newN.push([n[i][0], n[i][1]]);
     }
     return newN;
+}
+
+function getSpringForce(from, to) {
+    var d, f, cnx, cny, fx, fy;
+
+    d = dist(from, to);
+    JOINT_DIST = 0;
+    if (d > JOINT_DIST) {
+        f = (d - JOINT_DIST) * 0.05;
+        cnx = (to[0] - from[0]) / d;
+        cny = (to[1] - from[1]) / d;
+        fx = cnx * f;
+        fy = cny * f;
+
+        return [fx, fy];
+    }
+
+    return [0, 0];
 }
 
 var knots = new Knots();
